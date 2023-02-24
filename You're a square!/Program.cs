67f04fb850e646a4fa92e5d48b61e6cc874d0f73
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 13:51:51 +0200

    Add project files.

---
+++ You're a square!/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace You_re_a_square_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSquare(25));
+            Console.WriteLine(IsSquare(26));
+        }
+        public static bool IsSquare(int n)
+        {
+            if (n < 0)
+                return false;
+
+            if (n == 0)
+                return true;
+
+            int num = 0;
+            double dobNum = 0;
+            double result = 0;
+
+            num = Convert.ToInt32(Math.Sqrt(n));
+            dobNum = Math.Sqrt(n);
+            result = dobNum - num;
+
+            if (result == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
